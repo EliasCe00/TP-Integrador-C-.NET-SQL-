f59549246a6c90844a9a05d5f14ea46a9b8dd54c
Elias <eliasceballos2000@gmail.com>
2026-02-21 07:37:03 -0300

    Agrego las clases de la capa dominio

---
+++ TPFinalNivel2_Ceballos/dominio/Articulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    internal class articulo
+    {
+        public int Id { get; set; }
+
+        public string Codigo { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public string UrlImagen { get; set; }
+
+
+    }
+}
